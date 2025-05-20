6910a2ff68db4d251c6844f77c6d44639ff5b248
Denis Lessard <DenisLessard91@gmail.com>
2025-05-20 15:43:31 -0400

    fix padding top

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   devise_for :users, path: "",
     controllers: { 
       registrations: "users/registrations", 
-      omniauth_callbacks: "users/omniauth_callbacks" 
+      omniauth_callbacks: "users/omniauth_callbacks" ,
+      passwords: "passwords"
     },
     path_names: { 
       sign_in: "login", 
