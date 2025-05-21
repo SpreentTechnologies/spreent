aa288cf9357976a8833d76bc5f781c995f62ecf8
Denis Lessard <DenisLessard91@gmail.com>
2025-05-21 00:52:02 -0400

    test messages

--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -4,6 +4,15 @@ class ConversationChannel < ApplicationCable::Channel
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
+  end
+
+  def send_message(data)
+    Message.create!(
+      conversation_id: params[:conversation_id],
+      user_id: params[:user_id],
+      content: data["message"],
+      message_type: data["message_type"],
+    )
   end
 end
