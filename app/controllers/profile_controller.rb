d0000b261330525275a7271adf6ed431aad10704
Denis Lessard <DenisLessard91@gmail.com>
2025-05-21 02:24:57 -0400

    update bio in profile

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,4 +7,21 @@ class ProfileController < ApplicationController
   def show
 
   end
+
+  def update
+    # Update the profile of the current_user
+    @user = current_user
+    if @user.update(user_params)
+      flash[:success] = "Profile updated successfully"
+      redirect_to settings_path
+    else
+      flash[:error] = "Failed to update profile"
+      render :index
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :bio, :avatar)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -100,4 +100,6 @@ Rails.application.routes.draw do
 
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
+
+  post "/profile", to: "profile#update", as: :update_profile
 end
