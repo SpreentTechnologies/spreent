2c4ff57b55e4550de529dabb65ac14edc00d28ba
Denis Lessard <DenisLessard91@gmail.com>
2025-05-23 09:03:30 -0400

    push redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,17 +44,14 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-    config.cache_store = :redis_cache_store, {
-    url: ENV["REDIS_URL"],
-    pool_size: ENV.fetch("RAILS_MAX_THREADS", 5).to_i,
+  config.cache_store = :redis_cache_store, {
+    url: ENV['REDIS_URL'],
+    pool_size: 5,
     pool_timeout: 5,
     connect_timeout: 1,
     read_timeout: 1,
     write_timeout: 1,
-    reconnect_attempts: 3,
-    error_handler: ->(method:, returning:, exception:) {
-      Rails.logger.error("Redis cache error: #{exception.message}")
-    }
+    reconnect_attempts: 1
   }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
