93a70b9f9e6bda918e48089e64d7ac7727460db7
Denis Lessard <DenisLessard91@gmail.com>
2025-05-23 11:31:17 -0400

    yo

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -3,6 +3,8 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   connect() {
     this.swiper = new Swiper(".swiper-container", {
+      slidesPerView: 1.3,
+      spaceBetween: 1,
       // Swiper configuration options
       pagination: {
         el: ".swiper-pagination",
